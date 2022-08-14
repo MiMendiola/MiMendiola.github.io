646b5149b0c1c51bfddf526392c49aaa5ae6d80f
MiMendiola <mglmendiol@gmail.com>
2022-08-14 16:49:49 -0700

    Php Update

--- assets/js/main.js
+++ assets/js/main.js
@@ -74,3 +74,9 @@ function scrollUp(){
     if(this.scrollY >= 560) scrollUp.classList.add('show-scroll'); else scrollUp.classList.remove('show-scroll')
 }
 window.addEventListener('scroll', scrollUp)
+
+
+/*==================== SEND EMAIL ====================*/
+
+
+
