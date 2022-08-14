1877ea695bf90c7dea9cd7794bb28f76af771200
MiMendiola <mglmendiol@gmail.com>
2022-08-14 16:56:22 -0700

    Php Update

--- assets/php/main.php
+++ assets/php/main.php
@@ -16,33 +16,6 @@
 
     mail($to,$email_subject,$email_body,$headers);
 
-function IsInjected($str){
-    $injections = array('(\n+)',
-           '(\r+)',
-           '(\t+)',
-           '(%0A+)',
-           '(%0D+)',
-           '(%08+)',
-           '(%09+)'
-           );
-               
-    $inject = join('|', $injections);
-    $inject = "/$inject/i";
-    
-    if(preg_match($inject,$str))
-    {
-      return true;
-    }
-    else
-    {
-      return false;
-    }
-}
-
-if(IsInjected($visitor_email))
-{
-    echo "Bad email value!";
-    exit;
-}
+    header('Location: index.html');
 
 ?>
